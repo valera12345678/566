d60a632fb3bf690b006acf751e44ccc86d899e07
valera12345678 <valer4ik1304@gmail.com>
2025-05-18 21:56:43 +0300

    Add files via upload

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,35 +1,43 @@
 using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using System.Collections.Generic;
+using demo3103.Context;
+using Avalonia.Interactivity;
 using System.Linq;
+using demo3103.Models;
+using demo2603;
+using Castle.Components.DictionaryAdapter.Xml;
+using Avalonia.Media.Imaging;
+using Avalonia.Media;
+using System.Security.Cryptography.X509Certificates;
+using demo2603.Helper;
 
-namespace dem03;
-
+namespace demo3103;
 
 public partial class MainWindow : Window
 {
+    public List<Partner> partners = Helper.Database.Partners.ToList();
     public MainWindow()
-
     {
         InitializeComponent();
+        Listbox_Partner.ItemsSource = partners;
+        ProdList();
     }
-
-    private void LogClick(object? sender, RoutedEventArgs e)
+   
+    public void ProdList()
     {
-       
-        var Pass = Password.Text;
-
-        var user = Helper.Helper.Database.Admins.FirstOrDefault(x => x.Password == Pass);
-
-        if (user != null)
-        {
-
-            var window = new Services();
-            window.Show();
-            Close();
-        }
-
+       partners = Helper.Database.Partners.ToList();
     }
+    private void MenuItem_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        var redactirovanie = Listbox_Partner.SelectedItem as Partner;
 
+        AddEditWindow redakt = new(redactirovanie);
+        redakt.ShowDialog(this);
+        redakt.Closed += (a, sd) => ProdList();
+    }
+   
 }
+
+
